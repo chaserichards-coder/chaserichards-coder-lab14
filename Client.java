0191695f11d9c4a11e3214d2ec164a66670c562e
Maxwell Chase Richards <mrichards28@Maxwells-MacBook-Pro.local>
2025-11-17 00:26:27 -0500

    need to push this code to git

--- Client.java
+++ Client.java
@@ -1,5 +1,30 @@
-
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.time.*;
 
 public class Client {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    public Client(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
     
+    public Socket getSocket(){
+        return(this.socket);
+    }
+    public void handshake(){
+        out.println("12345");
+        out.flush();
+
+    }
+    public String request(String msg){
+        out.println(msg);
+        
+
+    }
+
 }

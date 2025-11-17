27be76f9d1840a33a0d0ccbb79d99d6559a8dd69
Maxwell Chase Richards <mrichards28@Maxwells-MacBook-Pro.local>
2025-11-17 03:06:48 -0500

    finished writing code for server need to push to git

--- Client.java
+++ Client.java
@@ -1,36 +1,33 @@
-import java.util.*;
 import java.io.*;
 import java.net.*;
-import java.time.*;
 
 public class Client {
     private Socket socket;
-    private PrintWriter out;
     private BufferedReader in;
-    public Client(String host, int port) throws IOException {
-        this.socket = new Socket(host, port);
-        this.out = new PrintWriter(socket.getOutputStream(), true);
-        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    }
-    
-    public Socket getSocket(){
-        return(this.socket);
-    }
-    public void handshake(){
-        out.println("12345");
-        out.flush();
+    private PrintWriter out;
 
+    public Client(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
-    public String request(String msg)throws IOException{
-        out.println(msg);
-        out.flush();
+
+    public void handshake() throws IOException {
+        out.println("12345");
+    }
+
+    public String request(String message) throws IOException {
+        out.println(message);
         return in.readLine();
     }
-    public void disconnect() throws IOException{
-        in.close();
-        out.close();
-        socket.close();
+
+    public Socket getSocket() {
+        return socket;
     }
 
-
+    public void disconnect() throws IOException {
+        if (in != null) in.close();
+        if (out != null) out.close();
+        if (socket != null) socket.close();
+    }
 }
--- Server.java
+++ Server.java
@@ -4,22 +4,116 @@ import java.net.*;
 import java.time.*;
 
 public class Server {
-private ServerSocket serverSocket;
-private List<ClientHandler> clients = new ArrayList<>();
-private List<LocalDateTime> connectedTimes = new ArrayList<>();
-public Server(int port) throws IOException {
-    serverSocket = new ServerSocket(port);
-}
-public void serve(int clients){
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients = new ArrayList<>();
+    private List<LocalDateTime> connectedTimes = new ArrayList<>();
 
-}
-public void disconnect(){
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
 
-}
-public List<LocalDateTime> getConnectedTimes() {
-    return ConnectedTimes;
-}
-private class ClientHandler{
+    public void serve(int n) {
+        for (int i = 0; i < n; i++) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler handler = new ClientHandler(clientSocket);
+                clients.add(handler);
+                handler.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-}
+    public void disconnect() {
+        try {
+            for (ClientHandler c : clients) {
+                c.interrupt();
+                c.close();
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+        synchronized (connectedTimes) {
+            ArrayList<LocalDateTime> copy = new ArrayList<>(connectedTimes);
+            Collections.sort(copy);
+            return copy;
+        }
+    }
+
+    private class ClientHandler extends Thread {
+        private Socket sock;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public ClientHandler(Socket sock) {
+            this.sock = sock;
+            try {
+                sock.setSoTimeout(5000);
+                this.out = new PrintWriter(sock.getOutputStream(), true);
+                this.in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+                String handshake = in.readLine();
+
+                if (!"12345".equals(handshake)) {
+                    out.println("couldn't handshake");
+                    return;
+                }
+
+                synchronized (connectedTimes) {
+                    connectedTimes.add(LocalDateTime.now());
+                }
+
+                String msg;
+                while (!Thread.interrupted() && (msg = in.readLine()) != null) {
+                    try {
+                        long num = Long.parseLong(msg);
+
+                        if (num > Integer.MAX_VALUE || num < 1) {
+                            out.println("There was an exception on the server");
+                        } else {
+                            long factors = countFactors(num);
+                            out.println("The number " + num + " has " + factors + " factors");
+                        }
+                    } catch (NumberFormatException e) {
+                        out.println("There was an exception on the server");
+                    }
+                }
+            } catch (java.net.SocketTimeoutException e) {
+                // Timeout - exit gracefully
+            } catch (Exception e) {
+                // Client disconnected
+            } finally {
+                close();
+            }
+        }
+
+        private long countFactors(long n) {
+            long count = 0;
+            for (long i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    count += (i * i == n) ? 1 : 2;
+                }
+            }
+            return count;
+        }
+
+        public void close() {
+            try {
+                if (sock != null) sock.close();
+                if (in != null) in.close();
+                if (out != null) out.close();
+            } catch (Exception ignored) {}
+        }
+    }
 }
